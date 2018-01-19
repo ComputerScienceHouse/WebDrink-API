f7896cc8bf11967354106ad059f6df32351671f6
Devin Matte <devinmatte@gmail.com>
2018-01-18 22:46:05 -0500

    Basic LDAP

--- src/utils/LDAP.php
+++ src/utils/LDAP.php
@@ -6,19 +6,25 @@ namespace WebDrinkAPI\Utils;
 use Exception;
 
 class LDAP {
+    private $userDn;
+    private $conn;
+
+    public function __construct($conn, $userDn) {
+        $this->conn = $conn;
+        $this->userDn = $userDn;
+    }
+
     function ldap_lookup($uid, $fields = null) {
-        global $conn;
-        global $userDn;
         try {
             // Make the search
             $filter = "(uid=" . $uid . ")";
             if (is_array($fields))
-                $search = ldap_search($conn, $userDn, $filter, $fields);
+                $search = ldap_search($this->conn, $this->userDn, $filter, $fields);
             else
-                $search = ldap_search($conn, $userDn, $filter);
+                $search = ldap_search($this->conn, $this->userDn, $filter);
             // Grab the results
             if ($search)
-                return ldap_get_entries($conn, $search);
+                return ldap_get_entries($this->conn, $search);
             else
                 return false;
         } catch (Exception $e) {
@@ -31,18 +37,16 @@ class LDAP {
     }
 
     function ldap_lookup_ibutton($ibutton, $fields = null) {
-        global $conn;
-        global $userDn;
         try {
             // Make the search
             $filter = "(ibutton=" . $ibutton . ")";
             if (is_array($fields))
-                $search = ldap_search($conn, $userDn, $filter, $fields);
+                $search = ldap_search($this->conn, $this->userDn, $filter, $fields);
             else
-                $search = ldap_search($conn, $userDn, $filter);
+                $search = ldap_search($this->conn, $this->userDn, $filter);
             // Grab the results
             if ($search)
-                return ldap_get_entries($conn, $search);
+                return ldap_get_entries($this->conn, $search);
             else
                 return false;
         } catch (Exception $e) {
@@ -51,13 +55,11 @@ class LDAP {
     }
 
     function ldap_update($uid, $replace) {
-        global $conn;
-        global $userDn;
         try {
             // Form the dn
-            $dn = "uid=" . $uid . "," . $userDn;
+            $dn = "uid=" . $uid . "," . $this->userDn;
             // Make the update
-            return ldap_mod_replace($conn, $dn, $replace);
+            return ldap_mod_replace($this->conn, $dn, $replace);
         } catch (Exception $e) {
             return false;
         }
