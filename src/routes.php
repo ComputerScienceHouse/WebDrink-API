365738877195b5d03a80002fe13653c61ad4b727
Devin Matte <devinmatte@gmail.com>
2018-03-21 10:55:19 -0400

    Trying to debug in Openshift

--- src/routes.php
+++ src/routes.php
@@ -17,38 +17,37 @@
 use Slim\Http\Request;
 use Slim\Http\Response;
 use WebDrinkAPI\Middleware\AuthMiddleware;
-use WebDrinkAPI\Utils\OIDC;
 
 $app->get('/', function (Request $request, Response $response) {
-    # TODO: Create fancy display page that shows to documentation
-    return $response->withJson("Welcome to the Webdrink API!");
+    // TODO: Create fancy display page that shows to documentation
+    return $response->withJson([__DIR__, __DIR__ . '/src/api/v2/test.php'], 200, JSON_PRETTY_PRINT);
 });
 
 $app->group('/v2', function () use ($app) {
     $auth = new AuthMiddleware();
 
     $app->group('/test', function () use ($app) {
-        include __DIR__ . '/api/v2/test.php';
+        require __DIR__ . '/api/v2/test.php';
     });
 
     $app->group('/users', function () use ($app) {
-        include __DIR__ . '/api/v2/users.php';
+        require __DIR__ . '/api/v2/users.php';
     })->add($auth);
 
     $app->group('/machines', function () use ($app) {
-        include __DIR__ . '/api/v2/machines.php';
+        require __DIR__ . '/api/v2/machines.php';
     })->add($auth);
 
     $app->group('/items', function () use ($app) {
-        include __DIR__ . '/api/v2/items.php';
+        require __DIR__ . '/api/v2/items.php';
     })->add($auth);
 
     $app->group('/temps', function () use ($app) {
-        include __DIR__ . '/api/v2/temps.php';
+        require __DIR__ . '/api/v2/temps.php';
     })->add($auth);
 
     $app->group('/drops', function () use ($app) {
-        include __DIR__ . '/api/v2/drops.php';
+        require __DIR__ . '/api/v2/drops.php';
     })->add($auth);
 });
 
